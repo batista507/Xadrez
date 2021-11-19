9708d96cc58e843af5faf79227030415bc16afe8
Willian Batista <willianbatista777.wb@gmail.com>
2021-11-18 23:45:31 -0300

    Criando a classe Posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-
+using tabuleiro;
 namespace xadrez_console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Posicao posicao = new Posicao(3, 4);
+
+            Console.WriteLine(posicao);
         }
     }
 }
---
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -0,0 +1,21 @@
+﻿namespace tabuleiro
+{
+    class Posicao
+    {
+        public int Liha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int liha, int coluna)
+        {
+            Liha = liha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return Liha
+                + ", "
+                + Coluna;
+        }
+    }
+}
